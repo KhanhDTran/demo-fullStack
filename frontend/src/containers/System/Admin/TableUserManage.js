c044a75815b029d21e0c2472419cc6a1b93741b2
KhanhDTran <trandanhkhanh@gmail.com>
2022-12-15 18:39:10 +0700

    65 model markdown

--- frontend/src/containers/System/Admin/TableUserManage.js
+++ frontend/src/containers/System/Admin/TableUserManage.js
@@ -2,9 +2,19 @@ import * as actions from "../../../store/actions"
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './TableUserManage.scss'
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+import 'react-markdown-editor-lite/lib/index.css';
+
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+function handleEditorChange({ html, text }) {
+    console.log('handleEditorChange', html, text);
+}
 
 
 class TableUserManage extends Component {
+
+
 
     constructor(props) {
         super(props)
@@ -37,46 +47,55 @@ class TableUserManage extends Component {
     render() {
         let users = this.state.uesrsRedux
         return (
-            <div className='container mb-5'>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Email</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Address</th>
-                            <th>Phone Number</th>
-                            <th>Gender</th>
-                            <th>Role</th>
-                            <th>Position</th>
-                            {/* <th>Image</th> */}
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users && users.map((item, index) => {
-                            return (
-                                <tr key={index}>
-                                    <th >{item.email}</th>
-                                    <th >{item.firstName}</th>
-                                    <th >{item.lastName}</th>
-                                    <th >{item.address}</th>
-                                    <th >{item.phoneNumber}</th>
-                                    <th >{item.gender}</th>
-                                    <th >{item.roleId}</th>
-                                    <th >{item.positionId}</th>
-                                    {/* <th >{item.image}</th> */}
-                                    <th ><button onClick={() => this.updateUser(item)}>update</button>
-                                        <button onClick={() => this.deleteUser(item)}>Delete</button>
-                                    </th>
-                                </tr>
-                            )
-                        }
-                        )}
+            <React.Fragment>
+                <div className='container mb-5'>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Email</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Address</th>
+                                <th>Phone Number</th>
+                                <th>Gender</th>
+                                <th>Role</th>
+                                <th>Position</th>
+                                {/* <th>Image</th> */}
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users && users.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <th >{item.email}</th>
+                                        <th >{item.firstName}</th>
+                                        <th >{item.lastName}</th>
+                                        <th >{item.address}</th>
+                                        <th >{item.phoneNumber}</th>
+                                        <th >{item.gender}</th>
+                                        <th >{item.roleId}</th>
+                                        <th >{item.positionId}</th>
+                                        {/* <th >{item.image}</th> */}
+                                        <th ><button onClick={() => this.updateUser(item)}>update</button>
+                                            <button onClick={() => this.deleteUser(item)}>Delete</button>
+                                        </th>
+                                    </tr>
+                                )
+                            }
+                            )}
 
-                    </tbody>
-                </table>
-            </div>
+                        </tbody>
+                    </table>
+
+
+
+                </div>
+                <MdEditor style={{ height: '500px' }}
+                    renderHTML={text => mdParser.render(text)}
+                    onChange={handleEditorChange} />
+            </React.Fragment>
+
         );
     }
 
